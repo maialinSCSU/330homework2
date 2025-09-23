8eeec6af4998e669891e4760451aadcf7d26bbe9
Maia Lin <lins4@southernct.edu>
2025-09-23 00:10:46 +0000

    Final draft, hallelujah

--- app.js
+++ app.js
@@ -24,17 +24,16 @@ let serverObj =  http.createServer(function(req,res){
         //array of valid days and times
         let validDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
         let validTimes = ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"];
-        //check if urlObj is a valid day
+        //check if urlObj is valid
          if (!(urlObj.query.name && urlObj.query.day && urlObj.query.time)) {
-
-                        error(res,400,"Missing query string");
-            //console.log("Missing query string");
+                error(res,400,"Missing query string");
+        //checks if url has valid dates and times
       }else if (!(validDays.includes(urlObj.query.day) && validTimes.includes(urlObj.query.time)))
         {
             error(res, 400, "Invalid query entry");
-            //console.log("Invalid query entry");
         }
         else {
+            //redirects to desired function or returns error
             switch (urlObj.pathname) {
                     case "/schedule":
                             schedule(urlObj.query,res);
@@ -46,7 +45,7 @@ let serverObj =  http.createServer(function(req,res){
                             check(urlObj.query, res);
                             break;
                     default:
-                            error(res,404,"pathname unknown");
+                            error(res,404,"pathname unknown"); //ex: /update would trigger this
 
     } }
 });
@@ -54,21 +53,19 @@ let serverObj =  http.createServer(function(req,res){
 function schedule(qObj,res) {
         if (availableTimes[qObj.day].some(time => time == qObj.time))
         {
-                //all works until you reach res
+                //updates the relevant day's respective array of time using .filter
                 availableTimes[qObj.day] = availableTimes[qObj.day].filter(item => item !== qObj.time);
-
+                //creates new var to push to the appointments array
                 let newAppt = {name: qObj.name, day: qObj.day, time: qObj.time};
 
                 appointments.push(newAppt);
-                //currently experiening issues with res
+                //IT WORKS YIPEEE
                 res.writeHead(200,{'content-type':'text/plain'});
                 res.write("scheduled");
                 res.end();
-                // console.log("scheduled");
         }
         else {
                 error(res,400,"Can't schedule");
-                // console.log("cant schedule");
         }
 }
 
@@ -76,22 +73,22 @@ function cancel(qObj, res) {
 
         if (appointments.some(appt => appt.name === qObj.name && appt.day === qObj.day && appt.time === qObj.time))
         {
+        //changed the apointments array so we could iterate upon it
 		appointments = appointments.filter(appt => !(appt.name === qObj.name && appt.day === qObj.day && appt.time === qObj.time));
                 availableTimes[qObj.day].push(qObj.time);
                 res.writeHead(200,{'content-type':'text/plain'});
                 res.write("Appointment has been cancelled");
                 res.end();
-                // console.log("appointment has been cancellled");
         }
         else {
                 res.writeHead(200,{'content-type':'text/plain'});
                 res.write("Appointment not found");
                 res.end();
-                // console.log("appointment not found")
         }
 }
 
 function check(qObj, res) {
+        //checks if the time n date in url are in availableTimes array
         if (availableTimes[qObj.day].some(time => time == qObj.time))//works
         {
                 res.writeHead(200,{'content-type':'text/plain'});
@@ -113,4 +110,4 @@ function error(response,status,message) {
         response.end();
 }
 
-serverObj.listen(80,function(){console.log("listening on port 80")});
+serverObj.listen(80,function(){console.log("listening on port 80")})
